51e59b530a41b15627928119aeec20f2d794f085
CaptainBigButt <vandocaster@gmail.com>
2019-09-25 14:00:42 -0400

    Token Update

--- main/config.js
+++ main/config.js
@@ -6,7 +6,7 @@ module.exports = class Config {
     this.ownerID = option.ownerID || "107047426992390144";
 	//this.ownerID = option.ownerID || "625470478886240268";
     //this.token = option.token || process.env.token;
-	this.token = 'NjI1NDcwNDc4ODg2MjQwMjY4.XYuplg.FDq4hhjRTTm_zpDO-Xg0yWswlqY';
+	this.token = 'NjI1NDcwNDc4ODg2MjQwMjY4.XYurKw.Qv8rXS6Z1fHKodMSvbGQJNh9P-g';
     //this.dbURL = option.dbURL || process.env.dbURL;
   }
 }
