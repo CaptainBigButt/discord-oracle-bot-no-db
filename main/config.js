6e68cab1295b6d7db923152af6629020e2acba38
CaptainBigButt <vandocaster@gmail.com>
2019-09-25 13:47:59 -0400

    Token Update

--- main/config.js
+++ main/config.js
@@ -1,12 +1,12 @@
 module.exports = class Config {
   constructor(option) {
     option = option || {};
-    this.prefix = option.prefix || "$";
+    this.prefix = option.prefix || "!";
     this.selfbot = option.selfbot || false;
     this.ownerID = option.ownerID || "107047426992390144";
 	//this.ownerID = option.ownerID || "625470478886240268";
     //this.token = option.token || process.env.token;
-	this.token = 'NjI1NDcwNDc4ODg2MjQwMjY4.XYuDBQ.1Ekk2VTpJK8vB-fBl99vkVqBjY8';
+	this.token = 'NjI1NDcwNDc4ODg2MjQwMjY4.XYunvA.ByZcz8R0Ha_Y5kVj68v0ApPYSB4';
     //this.dbURL = option.dbURL || process.env.dbURL;
   }
 }
